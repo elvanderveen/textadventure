2de8a98a42321ec619cc1fdb1a6ff0ffcfcd0ff9
Luís van der Veen <luisvdveen@gmail.com>
2018-06-04 10:22:10 +0200

    first commit

---
+++ Zuuul/Zuuul/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZuulCS
+{
+    class Player
+    {
+        private Room currentRoom;
+
+        public Player() {
+
+        
+        }
+
+        public Room setCurrentRoom(Room room)
+        {
+            this.currentRoom = room;
+            return room;
+        }
+        public Room getCurrentRoom()
+        {
+            return this.currentRoom;
+        }
+
+    }
+}
